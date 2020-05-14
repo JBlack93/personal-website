15229a40f8502b6a69d2aa00e5058169214f29ec
James Black <james.a.black@durham.ac.uk>
2020-05-14 18:36:07 +0100

    Added basic pages from backup. Images need to come next

--- index.js
+++ index.js
@@ -8,29 +8,38 @@ const expressSession = require('express-session');
 const connectMongo = require('connect-mongo');
 const connectFlash = require("connect-flash");
 
-const createPostController = require('./controllers/createPost')
-const homePageController = require('./controllers/homePage')
-const storePostController = require('./controllers/storePost')
-const getPostController = require('./controllers/getPost')
+const app = new express();
+
+const storePost = require('./middleware/storePost');
+const auth = require("./middleware/auth");
+const redirectIfAuthenticated = require('./middleware/redirectIfAuthenticated');
+
+const logoutController = require("./controllers/logout");
+const createPostController = require('./controllers/createPost');
+const homePageController = require('./controllers/homePage');
+const aboutPageController = require('./controllers/about');
+const workPageController = require('./controllers/work');
+const experienceController = require('./controllers/experience');
+const recentTalksController = require('./controllers/recent-talks');
+const blogController = require('./controllers/blog');
+const storePostController = require('./controllers/storePost');
+const getPostController = require('./controllers/getPost');
 const createUserController = require("./controllers/createUser");
 const storeUserController = require('./controllers/storeUser');
 const loginController = require("./controllers/login");
 const loginUserController = require('./controllers/loginUser');
 
-const app = new express();
 
-app.use(expressSession({
-    secret: 'secret'
-}));
-
-mongoose.connect('mongodb://localhost:27017/node-blog', { useNewUrlParser: true })
+mongoose.connect('mongodb://127.0.0.1:27017/node-blog', { useNewUrlParser: true,  useUnifiedTopology: true  })
     .then(() => 'You are now connected to Mongo!')
     .catch(err => console.error('Something went wrong', err))
 
 const mongoStore = connectMongo(expressSession);
  
 app.use(expressSession({
-    secret: 'secret',
+    resave: 'false',
+    saveUninitialized: 'false',
+    secret: 'UJBbi;l8sfs9OibkljN@)}-(FHJVk7P&*89;',
     store: new mongoStore({
         mongooseConnection: mongoose.connection
     })
@@ -51,12 +60,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(connectFlash());
 
-const storePost = require('./middleware/storePost')
-const auth = require("./middleware/auth");
-const redirectIfAuthenticated = require('./middleware/redirectIfAuthenticated')
-const logoutController = require("./controllers/logout");
-
 app.get("/", homePageController);
+app.get("/about", aboutPageController);
+app.get("/my-work", workPageController);
+app.get("/experience", experienceController);
+app.get("/recent-talks", recentTalksController);
+app.get("/blog", blogController);
 app.get("/post/:id", getPostController);
 app.get("/posts/new", auth, createPostController);
 app.post("/posts/store", auth, storePost, storePostController);
